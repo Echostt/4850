dfa64bac13f0cac7618ba9eb9b5703443af1ced4
echostt <echohoward05@gmail.com>
2018-05-10 15:51:14 -0600

    Project1 part 2 up to GameManager
    Completed the steps up to 14: Game Manager.
    
    Character moves, raycasts are generated from the mouse, physics layers are applied and the head bobbles.

---
+++ Project1/Assets/Scripts/Gun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+    public GameObject bulletPrefab;
+    public Transform launchPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+            if (!IsInvoking("fireBullet"))
+                InvokeRepeating("fireBullet", 0, 0.1f);
+
+        if (Input.GetMouseButton(1))
+            CancelInvoke("fireBullet");
+	}
+
+    void fireBullet() {
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        bullet.transform.position = launchPosition.position;
+        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+    }
+
+}
